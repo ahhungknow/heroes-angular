952de01e789810e255fa8811057e14710963cdc2
Nguyen Van Quang <39783340+ahhungknow@users.noreply.github.com>
2019-03-26 14:33:23 +0700

    Done

--- angular-tuor-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tuor-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -3,6 +3,7 @@ import{Hero} from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService }  from '../hero.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
@@ -25,5 +26,8 @@ export class HeroDetailComponent implements OnInit {
   }
   goBack():void{
     this.location.back();
+  }
+  save():void{
+    this.heroService.updateHero(this.hero).subscribe(()=>this.goBack());
   }
 }
